289f8160018e1244505ff508c19fbe36da47e451
csurbier <christophe@idevotion.fr>
2016-07-15 18:05:07 +0200

    utlisation variable environnement APP_HOME

--- citiesaround_wsgi.py
+++ citiesaround_wsgi.py
@@ -1,7 +1,7 @@
 # *coding: utf-8*
 import sys
 import os
-sys.path.append("/home/bas/app_76b49ba4-9531-497c-a210-b553dd7ec172")
+sys.path.append(os.getenv('APP_HOME'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'citiesaround.settings'
 os.environ['PYTHON_EGG_CACHE'] = '/tmp/.python-egg'
 from django.core.wsgi import get_wsgi_application
